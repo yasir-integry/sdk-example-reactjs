67fbe4ff9466c6482cf5c2d37c516f8240defadf
Osama Aamer <osama.aamer95@gmail.com>
2021-10-05 19:24:18 +0500

    Add SDK code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { IntegryJS, Helpers } from "@integry/sdk";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const init = async () => {
+      // Fill these in from the SDK deployment page
+      const appKey = "<APP_KEY>";
+      const appSecret = "<APP_SECRET>";
+      const userId = "<USER_ID>";
+      const deploymentId = "<DEPLOY_ID>";
+
+      // This is intended for use in testing only!
+      // Please refer to sdk.integry.io for more details
+      const hash = await Helpers.getAuthHash(userId, appSecret);
+
+      const integryHandle = new IntegryJS({
+        appKey,
+        hash,
+        userId,
+        deploymentId,
+      });
+
+      integryHandle.init({
+        containerId: "my-sdk-container",
+        renderMode: IntegryJS.RenderModes.INLINE,
+      });
+    };
+    init();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
       </header>
+      <div className='App-embed' id='my-sdk-container' />
     </div>
   );
 }
